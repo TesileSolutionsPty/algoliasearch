2be024073ef046a73ad02382dcaa8342ca876137
TesileSolutionsPty <info@tesilesolutions.co.za>
2025-02-20 12:30:28 +0200

    index.js

--- index.js
+++ index.js
@@ -1,34 +1,18 @@
-import algoliasearch from "algoliasearch/lite";
-import instantsearch from "instantsearch.js";
-import { searchBox, hits, configure, poweredBy } from "instantsearch.js/es/widgets";
-
-// Initialize Algolia
-const searchClient = algoliasearch('18ZO40UVWY', 'd3ee094323bd311dccdd54c7dbb933b6');
+// Use global variables from CDN
+const searchClient = algoliasearch("18ZO40UVWY", "d3ee094323bd311dccdd54c7dbb933b6");
 
 const search = instantsearch({
-  indexName: 'users',
+  indexName: "users",
   searchClient,
 });
 
-// Add search widgets
 search.addWidgets([
-  searchBox({
-    container: "#searchbox",
-    placeholder: "Search for products...",
-  }),
-  hits({
+  instantsearch.widgets.searchBox({ container: "#searchbox" }),
+  instantsearch.widgets.hits({
     container: "#hits",
     templates: {
-      item: (hit) => `
-        <div>
-          <strong>${hit.name}</strong><br>
-          ${hit.description}
-        </div>
-      `,
+      item: (hit) => `<div><strong>${hit.name}</strong><br>${hit.description}</div>`,
     },
-  }),
-  poweredBy({
-    container: "#algolia-footer",
   }),
 ]);
 
